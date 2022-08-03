2e9523a05db07954c98389f3362e466779275beb
root <root@LAPTOP-6QJHLG50.localdomain>
2022-08-03 21:58:23 +0930

    p2

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -4,7 +4,11 @@ void print_scaled(int array[3][3],int scale){
 	for(int i=0;i<3;i++){
 		for(int j=0;j<3;j++){
 			int a=array[i][j]*scale;
-			cout<<a<<" ";
+			if(j==3){
+				cout<<a;
+			}else{
+				cout<<a<<" ";
+			}
 			
 		}
 		cout<<endl;
