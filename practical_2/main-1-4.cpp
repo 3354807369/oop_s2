3ae1e909078d25ccf73506376606b58031d69bf0
root <root@LAPTOP-6QJHLG50.localdomain>
2022-08-03 21:35:59 +0930

    p2

--- practical_2/main-1-4.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-
-extern void print_scaled(int array[3][3],int scale);
-
-int main(){
-	int array[3][3]={{1,2,3},{3,4,5},{4,5,6}};
-	int number=3;
-	print_scaled(array,number);
-}
--- practical_2/main-2-1.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-
-extern void print_binary_str(std::string decimal_number);
-
-int main(){
-	std::string decimal_number=("75");
-	print_binary_str(decimal_number);
-}
