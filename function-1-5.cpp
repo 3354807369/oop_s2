0009d8e128fc56c1eea6dad196814ae42918d19f
root <root@LAPTOP-6QJHLG50.localdomain>
2022-08-03 22:01:44 +0930

    p2

--- function-1-5.cpp
+++ function-1-5.cpp
@@ -4,7 +4,7 @@ void print_summed(int array1[3][3],int array2[3][3]){
 	for(int i=0;i<3;i++){
 		for(int j=0;j<3;j++){
 			int a=array1[i][j]+array2[i][j];
-			if(j==3){
+			if(j==2){
 				cout<<a;
 			}else{
 				cout<<a<<" ";
