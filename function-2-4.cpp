dcbae09e2946fd92d7bfc073f916f4a4f1bf4cf8
root <root@LAPTOP-6QJHLG50.localdomain>
2022-08-15 23:06:37 +0930

    wk04

--- function-2-4.cpp
+++ function-2-4.cpp
@@ -1,5 +1,13 @@
 #include<iostream>
+float add_op(float left, float right){
+	float a=left+right;
+	return a;
+}
 
+float subtract_op(float left, float right){
+	float a=left-right;
+	return a;
+}
 float multiply_op(float left, float right){
 	float a=left*right;
 	return a;
