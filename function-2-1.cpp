436ee23f5d0e5dc26f2545f777fff01b9ed21dd8
root <root@LAPTOP-6QJHLG50.localdomain>
2022-08-15 22:31:19 +0930

    wk04

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -1,16 +1,16 @@
 #include<iostream>
 
 float add_op(float left, float right){
-	int a=left+right;
+	float a=left+right;
 	return a;
 }
 
 float subtract_op(float left, float right){
-	int a=left-right;
+	float a=left-right;
 	return a;
 }
 float arithmetic_ops(float left, float right, std::string op){
-	int a=0;
+	float a=0;
 	if(op=="add"){
 		 a=add_op(left,right);
 	}else if(op=="subtract"){
--- main-2-1.cpp
+++ main-2-1.cpp
@@ -4,8 +4,8 @@ extern float subtract_op(float left, float right);
 extern float arithmetic_ops(float left, float right, std::string op);
 
 int main(){
-	int left=5;
-	int right=4;
+	float left=5.5;
+	float right=4.3;
 	std::string op="subtract";
 	std::cout<<arithmetic_ops(left,right,op)<<std::endl;
 }
