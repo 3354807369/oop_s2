f56ee7a4c7a73da792ef2eeef8838ef7a92b03f7
root <root@LAPTOP-6QJHLG50.localdomain>
2022-08-16 13:46:55 +0930

    p3

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -6,8 +6,8 @@ void string_2d_copy(std::string first[][2], std::string second[][2], int n){
 			second[i][j]=first[i][j];
 		}
 	}
-	if(n%2==0){
-	for(int i=0;i<n/2;i++){
+	
+	for(int i=0;i<n;i++){
 		for(int j=0;j<2;j++){
 			std::cout<<second[i][j];
 			
@@ -15,23 +15,5 @@ void string_2d_copy(std::string first[][2], std::string second[][2], int n){
 		
 	}
 	std::cout<<std::endl;
-	}
-	if(n%2!=0){
-		for(int i=0;i<n/2+1;i++){
-			if(i==n-2){
-			for(int j=0;j<1;j++){
-				
-				std::cout<<second[i][j];
-			}
-			}else{
-				for(int j=0;j<2;j++){
-				
-				std::cout<<second[i][j];
-			}
-			}
-		}
-		std::cout<<std::endl;
-	}
-	
-	
 }
+
