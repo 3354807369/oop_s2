ca258e5074ce043f7a7744566b2c687bf0e4c4b8
root <root@LAPTOP-6QJHLG50.localdomain>
2022-08-16 19:19:42 +0930

    p3

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -2,15 +2,14 @@
 using namespace std;
 void copy_integers(int old_array[],int new_array[],int length){
 	int *p1;
+	p1=&old_array[0];
 	int *p2;
+	p2=&new_array[0];
 	for(int i=0;i<length;i++){
+		*(p2+i)=*(p1+i);
 		
-		p1=old_array;
-		p2=new_array;
-		*p1=*p2;
-		*p1++;
-		*p2++;
 	}
-	*p1='\0';
+	
+	
 	
 }
--- main-1-3.cpp
+++ main-1-3.cpp
@@ -1,9 +1,15 @@
 #include<iostream>
-
+using namespace std;
 extern void copy_integers(int old_array[],int new_array[],int length);
 
 int main(){
 	int a[5]={1,2,3,4,5};
 	int b[5];
 	copy_integers(a,b,5);
+	for(int i=0;i<5;i++){
+	
+		std::cout<<b[i];
+	
+	}
+	std::cout<<std::endl;
 }
