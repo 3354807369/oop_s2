244038b310636dafdf3db1a6f6b1d6480f9b94dd
root <root@LAPTOP-6QJHLG50.localdomain>
2022-08-16 13:38:49 +0930

    p3

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -6,10 +6,32 @@ void string_2d_copy(std::string first[][2], std::string second[][2], int n){
 			second[i][j]=first[i][j];
 		}
 	}
-	for(int i=0;i<n;i++){
+	if(n%2==0){
+	for(int i=0;i<n/2;i++){
 		for(int j=0;j<2;j++){
 			std::cout<<second[i][j];
+			
 		}
+		
 	}
 	std::cout<<std::endl;
+	}
+	if(n%2!=0){
+		for(int i=0;i<n/2+1;i++){
+			if(i==n-2){
+			for(int j=0;j<1;j++){
+				
+				std::cout<<second[i][j];
+			}
+			}else{
+				for(int j=0;j<2;j++){
+				
+				std::cout<<second[i][j];
+			}
+			}
+		}
+		std::cout<<std::endl;
+	}
+	
+	
 }
--- main-1-1.cpp
+++ main-1-1.cpp
@@ -3,7 +3,7 @@
 extern void string_2d_copy(std::string first[][2], std::string second[][2], int n);
 
 int main(){
-    std::string b[3][2]={"11","22","33"};
+    std::string b[3][2]={"11","22","33","44"};
 	std::string a[3][2];
 	string_2d_copy(b,a,3);
 	
