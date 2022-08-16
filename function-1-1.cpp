48936ddfe978038f56631aa956298f685da95612
root <root@LAPTOP-6QJHLG50.localdomain>
2022-08-16 12:50:21 +0930

    p3

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
-#include<string.h>
+
 void string_2d_copy(std::string first[][2], std::string second[][2], int n){
-	for(int i=0;i<n;i++){
+	for(int i=0;i<n-1;i++){
 		for(int j=0;j<2;j++){
 			second[i][j]=first[i][j];
 		}
--- main-1-1.cpp
+++ main-1-1.cpp
@@ -3,7 +3,8 @@
 extern void string_2d_copy(std::string first[][2], std::string second[][2], int n);
 
 int main(){
-    std::string b[3][2]={"ab","cd","ef"};
+    std::string b[3][2]={"11","22","33"};
 	std::string a[3][2];
 	string_2d_copy(b,a,3);
+	
 }
