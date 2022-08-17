26233884f8ad699b899aa481ac73941d9ed3245a
root <root@LAPTOP-6QJHLG50.localdomain>
2022-08-17 16:47:37 +0930

    p3

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -1,10 +1,9 @@
 #include<iostream>
 using namespace std;
 void copy_integers(int old_array[],int new_array[],int length){
-	int *p1;
-	p1=&old_array[0];
-	int *p2;
-	p2=&new_array[0];
+	int *p1=old_array;
+	
+	int *p2=new_array;
 	for(int i=0;i<length;i++){
 		*(p2+i)=*(p1+i);
 		
