f6b7d4aa14b18a55fe9b6700ec9be77102cf556a
root <root@LAPTOP-6QJHLG50.localdomain>
2022-08-24 16:52:35 +0930

    p2

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -1,13 +1,10 @@
 #include<iostream>
+extern int* left_shift(int* vals, int len, int amount);
+using namespace std;
 
- int *left_shift(int *vals, int len, int amount){
-    int* array=new int[6];
-	int j=amount;
-    for(int i=0;i<len;i++){
-	if(i>=amount){
-			array[i]=vals[j];
-			array[j]=0;
-	}
-	}
-	return array;
+int* left_shift(int* vals, int len, int amount) {
+ int* array = new int[6];
+ for (int i = 0, j = amount; j < len; i++, j++) {
+  array[i] = vals[j];
+  array[j] = 0;
  }
