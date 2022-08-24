77a1bbc16fda7cc13ff93170557e4bdd36919450
root <root@LAPTOP-6QJHLG50.localdomain>
2022-08-24 16:58:38 +0930

    p2

--- function-4-1.cpp
+++ function-4-1.cpp
@@ -1,5 +1,19 @@
-#include<iostream>
+#include <iostream>
 #include "function-4-1.h"
-#include<string>
 using namespace std;
-get::get
+BusCounter::BusCounter(int cap)
+{
+	BusCounter::bus_count = 0;
+	capacity = cap;
+}
+void BusCounter::passenger_board() {
+	if (bus_count < capacity)
+		bus_count++;
+}
+void BusCounter::passenger_exit() {
+	if (bus_count > 0)
+		bus_count--;
+}
+int BusCounter::get_count() {
+	return bus_count;
+}
