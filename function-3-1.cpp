c874033ec3cd7373167a1a243d6bfe0604fb664e
root <root@LAPTOP-6QJHLG50.localdomain>
2022-08-24 16:56:24 +0930

    p2

--- function-3-1.cpp
+++ function-3-1.cpp
@@ -1,13 +1,14 @@
+#include<string>
 #include<iostream>
+extern int integers_in_range(int vals[], int length, int bottom, int top);
+using namespace std;
 
-int integers_in_range(int vals[], int length, int bottom, int top){
-	
-	int count=0;
-	for(int i=0;i<length;i++){
-		if(i>=bottom-1&&i<=top-1){
-			count++;
-		}
-	}
-	return count;	  
-	}
-	
+int integers_in_range(int vals[], int length, int bottom, int top) {
+ int index = 0;
+ for (int i = 0; i < length; i++) {
+  if (vals[i] >= bottom && vals[i] <= top) {
+   index++;
+  }
+ }
+ return index;
+}
