3bc1b63be93da2b137c241d8e17e1c99b9cf1725
root <root@LAPTOP-6QJHLG50.localdomain>
2022-08-24 16:15:40 +0930

    p2

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -2,12 +2,12 @@
 
  int *left_shift(int *vals, int len, int amount){
     for(int i=0;i<len;i++){
-		if(*(vals+i)==amount){
-			for(int j=1;j<len-i;j++){
-			*(vals+j-1)=*(vals+i+j);
-		    *(vals+i+j)=0;
-			}
-		}
+		
+			
+			*(vals+i-amount)=*(vals+i);
+		    *(vals+i)=0;
+			
+		
 	}
 	return vals;
  }
