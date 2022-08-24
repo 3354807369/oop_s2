7c0feeacb7fbfbdfebb471636643d6276356f7be
root <root@LAPTOP-6QJHLG50.localdomain>
2022-08-24 16:43:27 +0930

    p2

--- function-3-1.cpp
+++ function-3-1.cpp
@@ -1,12 +1,13 @@
 #include<iostream>
 
 int integers_in_range(int vals[], int length, int bottom, int top){
-	int res=0;
+	
+	int count=0;
 	for(int i=0;i<length;i++){
 		if(i>=bottom-1&&i<=top-1){
 			count++;
 		}
 	}
-	return 7;	  
+	return count;	  
 	}
 	
