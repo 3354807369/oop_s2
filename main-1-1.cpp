4f59ee2f8ae1fbece019e590874f3b66ea73777a
root <root@LAPTOP-6QJHLG50.localdomain>
2022-08-24 16:10:33 +0930

    p2

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -3,7 +3,7 @@ using namespace std;
 extern  int *left_shift(int *vals, int len, int amount);
 
 int main(){
-	int vals[6]={5,4,3,2,1,1};
+	int vals[]={5,4,3,2,1,1};
 	int *sv=left_shift(vals,6,3);
 	for(int i=0;i<6;i++){
 		cout<<*(sv+i);
--- main-3-1.cpp
+++ main-3-1.cpp
@@ -5,8 +5,6 @@ extern int integers_in_range(int vals[], int length, int bottom, int top);
 int main(){
 	int vals[]={1,7,3,4,6,5,2};
 	int res=integers_in_range(vals,7,2,6);
-	cout<<res<<endl;
-	int val[]={3,5,2,4,3};
-	int ress=integers_in_range(vals,5,2,4);
-	cout<<ress<<endl;
+	
+
 }
