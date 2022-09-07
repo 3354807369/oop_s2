c11615ee6a8d6bbd305ddf7aca269790f1bd1aed
root <root@LAPTOP-6QJHLG50.localdomain>
2022-09-07 16:16:31 +0930

    a

--- Unit.cpp
+++ Unit.cpp
@@ -9,16 +9,20 @@ Unit::Unit(){
 Unit::Unit(int unit_val, int num_beds, double unit_size){
     if(unit_val<0){
         val=0;
+    }else{
+        val=unit_val;
     }
     if(num_beds<0){
         num=0;
+    }else{
+        num=num_beds;
     }
     if(unit_size){
         size=0;
+    }else{
+        size=unit_size;
     }
-    val=unit_val;
-    num=num_beds;
-    size=unit_size;
+
 }
 int Unit::get_num_bedrooms(){
     return num;
